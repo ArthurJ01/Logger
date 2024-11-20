f479f21cf79cce783b4fa15033b7ba05cf2ceca8
Arthur01 <arthur.jerlstrom@gmail.com>
2024-11-20 23:45:07 +0100

    Added tree cutting logic, tree can be chopped

--- Assets/Scripts/Player/TreeCutting.cs
+++ Assets/Scripts/Player/TreeCutting.cs
@@ -1,13 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class TreeCutting : MonoBehaviour
 {
 
     [SerializeField] private LayerMask treeLayer;
+    [SerializeField] private int damage;
 
     private bool inCuttingRange = false;
+    private GameObject currentTree;
+
+
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions(); // Create it once
+    }
+
+    private void OnEnable()
+    {
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.AxeHit.performed += OnAxeHitPerformed;
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.Player.AxeHit.performed -= OnAxeHitPerformed;
+        playerInputActions.Player.Disable();
+    }
+
+    private void OnAxeHitPerformed(InputAction.CallbackContext context)
+    {
+        if (inCuttingRange && currentTree != null)
+        {
+            currentTree.GetComponent<TreeLogic>().damageTree(damage);
+        }
+    }
+
+    private void OnAxeHitCanceled(InputAction.CallbackContext context)
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,6 +59,7 @@ public class TreeCutting : MonoBehaviour
         if (((1 << other.gameObject.layer) & treeLayer) != 0)
         {
             inCuttingRange = true;
+            currentTree = other.gameObject;
         }
     }
     private void OnTriggerExit(Collider other)
@@ -30,6 +67,7 @@ public class TreeCutting : MonoBehaviour
         if (((1 << other.gameObject.layer) & treeLayer) != 0)
         {
             inCuttingRange = false;
+            currentTree = null;
         }
     }
 }
--- Assets/Scripts/TreeLogic.cs
+++ Assets/Scripts/TreeLogic.cs
@@ -6,7 +6,7 @@ public class TreeLogic : MonoBehaviour
 {
 
     [SerializeField] private GameObject logPrefab;
-    [SerializeField] private int health;
+    [SerializeField] private int health = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +23,20 @@ public class TreeLogic : MonoBehaviour
 
     public void damageTree(int dmg)
     {
-        Debug.Log("tree damaged: " + dmg);
+        health--;
+        if (health <= 0)
+        {
+            spawnLog();
+        }
+
+        Debug.Log("tree damaged: " + dmg + ", health: " + health);
     }
 
     public void spawnLog()
     {
         Vector3 _treePosition = GetComponent<Transform>().position;
         Quaternion _logRotation = Quaternion.identity;
-        _logRotation.eulerAngles = new Vector3(90, 0, 0);
+        _logRotation.eulerAngles = new Vector3(70, 0, 0);
         Instantiate(logPrefab, _treePosition, _logRotation);
         
         Destroy(gameObject);
